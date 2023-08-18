241d87aa80a5e09fd2da28af05092e5c6f2c9003
BJ Boggs <rboggs@afbtalentlab.org>
2023-08-18 14:51:24 -0400

    More Concise code for render Drawer Screens

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React, {useEffect} from 'react';
-import { View, StyleSheet, StatusBar, AccessibilityInfo} from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, StatusBar, AccessibilityInfo, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { colors, def_Page } from './constant';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 //Components-----------------------------------------------------
 import { DrawerContent } from './components/DrawerContent';
@@ -20,8 +22,21 @@ import Contact from './Screens/Contact';
 
 const Drawer = createDrawerNavigator();
 
-function App() {  
-  //----------------------------------------
+function App() {
+
+  // Define an array of screen configurations
+  const screens = [
+
+    //name is the name of the screen set in def_Page if commonLabel is set to "false" it will display
+    //component is the name of the imported component under "Screens" in this file!
+
+    { name: def_Page.page1Name, component: HomeScreen },
+    { name: def_Page.page2Name, component: AltText },
+    { name: def_Page.page3Name, component: WorkScreen },
+    { name: def_Page.page4Name, component: ServicesScreen },
+    { name: def_Page.page5Name, component: Contact },
+  ];
+
   return (
     <View style={styles.container}>
       {/* Status Bar ------------------*/}
@@ -32,48 +47,31 @@ function App() {
       {/* -------------------------------- */}
       <NavigationContainer>
         <Drawer.Navigator
-          drawerContent={(props) => <DrawerContent {...props}/>}
+          drawerContent={(props) => <DrawerContent {...props} />}
           screenOptions={({ navigation }) => ({
             headerStyle: { backgroundColor: colors.headerBackground }, //background color is defined in ./constant.js
             headerTintColor: colors.titleColor, //Header color is defined in ./constant.js
             drawerPosition: 'right', //drawer will open from the right side (default left)
-            headerLeft: false, 
+            headerLeft: false,
             headerRight: () => <CustomHeaderRight navigation={navigation} />, //Custom Header Right
           })}
         >
-          {/* These are the screens displayed inside the drawer when opened */}
-          {/* def_Page is located in constant.js this is where you can set the drawer page name and page name */}
-          
-          {/* -------------------------------------------------------------------------------- */}
-          {/* if def_Page 'setCommonLabel' is true set the title to def_Page 'commonLabel' 
-          if false set to {} therefore instead taking the def_Page pageName */}
-
-          {/* options={def_Page.setCommonLabel ? { title: def_Page.commonLabel} : {}} */}
-          {/* -------------------------------------------------------------------------------- */}
-
-          <Drawer.Screen 
-            options={def_Page.setCommonLabel ? { title: def_Page.commonLabel} : {}}
-            name={def_Page.page1Name} 
-            component={HomeScreen}/>
-          <Drawer.Screen
-            options={def_Page.setCommonLabel ? { title: def_Page.commonLabel} : {}}
-            name={def_Page.page2Name}
-            component={AltText}/>
-          <Drawer.Screen             
-            options={def_Page.setCommonLabel ? { title: def_Page.commonLabel} : {}}
-            name={def_Page.page3Name} 
-            component={WorkScreen} />
-          <Drawer.Screen             
-            options={def_Page.setCommonLabel ? { title: def_Page.commonLabel} : {}}
-            name={def_Page.page4Name} 
-            component={ServicesScreen} />
-          <Drawer.Screen             
-            options={def_Page.setCommonLabel ? { title: def_Page.commonLabel} : {}}
-            name={def_Page.page5Name} 
-            component={Contact} />
-
-          {/* ------------------------------------------------------------- */}
-
+          {/* Map over the screens array to generate Drawer.Screen components */}
+          {screens.map((screen, index) => (
+            <Drawer.Screen
+              key={index}
+              options={def_Page.setCommonLabel ? {
+                title: (
+                  <View style={styles.headerContainer}>
+                    <FontAwesome name="universal-access" style={styles.headerIcon} /> 
+                    <Text style={styles.headerText}>{def_Page.commonLabel}</Text>
+                  </View>
+                )
+              } : {}}
+              name={screen.name}
+              component={screen.component}
+            />
+          ))}
         </Drawer.Navigator>
       </NavigationContainer>
     </View>
@@ -84,6 +82,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  headerContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  headerIcon:{
+    color: 'white',
+    marginRight: 10,
+    fontSize: 30,
+  }
+
 });
 
 export default App;
