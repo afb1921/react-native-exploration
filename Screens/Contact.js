d4618dc5187e69376662c93b14a75f8c3dc49e6a
BJ Boggs <rboggs@afbtalentlab.org>
2023-08-19 15:06:36 -0400

    Removed Used component

--- Screens/Contact.js
+++ Screens/Contact.js
@@ -3,22 +3,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 
-import useAccessibilityAnnouncement from '../components/useAccessibilityAnnouncement'; 
-
 function ContactScreen() {
-
-
-    // Accessibility Screen Change Notification
-
-    const announceWithPriority = useAccessibilityAnnouncement();
-
-    useFocusEffect(
-      React.useCallback(() => {
-        announceWithPriority('Contact Screen');
-      }, [announceWithPriority])
-    );
-
-
 
   return (
     <View>
