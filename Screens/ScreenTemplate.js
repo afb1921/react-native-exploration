aa1300c707df19890d4d22a590ea212c60688345
BJ Boggs <rboggs@afbtalentlab.org>
2023-08-23 12:50:11 -0400

    New Screen Template For development

---
+++ Screens/ScreenTemplate.js
@@ -0,0 +1,14 @@
+// ScreenTemplate.js
+import React from 'react';
+import { View, Text} from 'react-native';
+
+function ScreenTemplate() {
+  return (
+    <View>
+      <Text>Welcome to the Screen Template Screen!</Text>
+    </View>
+
+  );
+}
+
+export default ScreenTemplate;
