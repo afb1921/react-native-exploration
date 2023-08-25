a9c11a2016cba21e5bc32cbce60a913a746c8b88
BJ Boggs <rboggs@afbtalentlab.org>
2023-08-25 02:32:34 -0400

    Announcement for Menu Drawer Open

--- components/CustomHeaderRight.js
+++ components/CustomHeaderRight.js
@@ -1,14 +1,15 @@
-import React, {useContext} from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import { DrawerActions } from '@react-navigation/native';
+import React, {useContext, useState, useEffect} from 'react';
+import { TouchableOpacity, StyleSheet, AccessibilityInfo } from 'react-native';
+import { DrawerActions} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { def_Page } from '../constant';
 import { darkMode, lightMode } from '../Themes/defaultThemes';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+
 
 //Theme Imports---------------------------------------------------
 import themeContext from '../Themes/themeContext';
 //----------------------------------------------------------------
-
 
 const CustomHeaderRight = ({ navigation }) => {
 
@@ -16,9 +17,32 @@ const CustomHeaderRight = ({ navigation }) => {
   const theme = useContext(themeContext)
   //----------------------------------------------------------------
 
+  const handleMenuPress = () => {
+
+    navigation.dispatch(DrawerActions.openDrawer());
+
+    const delay = 200
+
+    setTimeout(() => {
+      // Announce the action to screen readers
+      AccessibilityInfo.announceForAccessibility('Opened Menu Drawer');
+      console.log("Opened Menu Drawer")
+    }, delay);
+    
+  };
+
+
   return (
-    <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
-      <Icon name="bars" size={30} style={[styles.hamburgerIcon, {color: theme.hamburgerIcon}]} accessibilityLabel='Menu Drawer' accessibilityRole='menu' />
+    <TouchableOpacity 
+      onPress={handleMenuPress}
+    >
+      <Icon 
+        name="bars" 
+        size={30} 
+        style={[styles.hamburgerIcon, {color: theme.hamburgerIcon}]} 
+        accessibilityLabel='Menu Drawer' 
+        accessibilityRole='menu' 
+      />
     </TouchableOpacity>
   );
 };
