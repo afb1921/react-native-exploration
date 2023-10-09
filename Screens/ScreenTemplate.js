24487e36f3952263528e08d1125763994aec442c
BJ Boggs <107263454+ThunderDragon32@users.noreply.github.com>
2023-10-09 13:52:14 -0400

    Updated Screen Template

--- Screens/ScreenTemplate.js
+++ Screens/ScreenTemplate.js
@@ -1,14 +1,73 @@
 // ScreenTemplate.js
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, { useRef, useContext } from 'react';
+import { View, Text, ScrollView, StyleSheet, AccessibilityInfo } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+
+
+import themeContext from '../Themes/themeContext';
+
 
 function ScreenTemplate() {
+
+  //Theme Manangement
+  //===============================================================
+  const { theme } = useContext(themeContext);
+  //===============================================================
+
+  // First Element Set Focus for Screen Reader & Reset Scroll View
+  //===============================================================
+  const firstElementRef = useRef(null);
+  const scrollViewRef = useRef(null);
+
+  //When the page loads (everytime) the useFocusEffect is triggered
+  //This is used to bring focus on the first element
+  useFocusEffect(
+    React.useCallback(() => {
+
+      console.log("use Focus Effect")
+
+      //Reset Scroll View to the top of the page
+      if (scrollViewRef.current) {
+        console.log("Scroll")
+        scrollViewRef.current.scrollTo({ y: 0 });
+      }
+
+      // // Add a time delay before executing the focus logic, 
+      // //This is important so the it gives it a chance to find the firstElement during loading.
+      const delay = 250; // Delay in milliseconds
+
+      setTimeout(() => {
+
+        if (firstElementRef.current) {
+          const reactTag = firstElementRef.current._nativeTag;
+          AccessibilityInfo.setAccessibilityFocus(reactTag);
+          console.log('First Element===========n\n'); //Debuging purposes
+
+        }
+      }, delay)
+    }, [])
+
+  )
+
   return (
-    <View>
-      <Text>Welcome to the Screen Template Screen!</Text>
-    </View>
+
+    <ScrollView ref={scrollViewRef} style={[styles.container, { backgroundColor: theme.page.contentBackground }]}>
+
+      <View>
+        <Text ref={firstElementRef}>Welcome to the Page 4 Screen!</Text>
+
+      </View>
+
+    </ScrollView>
+
+
 
   );
 }
+
+const styles = StyleSheet.create({
+
+
+});
 
 export default ScreenTemplate;
